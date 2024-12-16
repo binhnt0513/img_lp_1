8f5d5bfbf594b6712558d0fe1995df1532ca3e01
binhnt <binhnt.0513@gmail.com>
2024-12-16 19:59:09 +0700

    init

--- public/js/script.js
+++ public/js/script.js
@@ -1,46 +1,45 @@
 function toggleMenu() {
-  const menuIcon = document.getElementById("menuBtn");
-  const menu = document.getElementById("sideMenu");
-  menuIcon.classList.toggle("change");
-  menu.classList.toggle("open");
+    const menuIcon = document.getElementById("menuBtn");
+    const menu = document.getElementById("sideMenu");
+    menuIcon.classList.toggle("change");
+    menu.classList.toggle("open");
 
-  const body = document.body;
-  body.classList.toggle("no-scroll");
+    const body = document.body;
+    body.classList.toggle("no-scroll");
 }
 
-function navigateToSection(event) {
-  event.preventDefault();  // Prevent the default anchor behavior
 
-  const targetId = event.target.getAttribute("href");  // Get the href value
-  const targetSection = document.querySelector(targetId);  // Find the target section by ID
+function navigateToSection(event, targetId, offset = 80, isMenu = true) {
+    event.preventDefault();  // Prevent the default anchor behavior
 
-  if (targetSection) {
-    const offset = 200;
-    const elementPosition = targetSection.getBoundingClientRect().top + window.scrollY;
-    const targetPosition = elementPosition - offset;
-    // Scroll to the target section smoothly
-    window.scrollTo({ top: targetPosition, behavior: "smooth" });
-    // Scroll to the target section smoothly
-    targetSection.scrollIntoView({behavior: "smooth"});
+    const targetSection = document.querySelector(targetId);  // Find the target section by ID
+    if (targetSection) {
+        // const offset = 120;
+        const elementPosition = targetSection.getBoundingClientRect().top + window.scrollY;
+        const targetPosition = elementPosition - offset;
+        // Scroll to the target section smoothly
+        window.scrollTo({top: targetPosition, behavior: "smooth"});
 
-    // Close the menu after clicking
-    toggleMenu();
-  }
+        // Close the menu after clicking
+        if (isMenu) {
+            toggleMenu();
+        }
+    }
 }
 
 $(document).ready(function () {
-  if ($(window).width() <= 768) {
-    var $button = $('#stick-btn'); // Replace with your button's ID
+    if ($(window).width() <= 768) {
+        var $button = $('#stick-btn'); // Replace with your button's ID
 
-    $(window).scroll(function (event) {
-      var st = $(this).scrollTop();
-      if (st > 0) {
-        // Downscroll or not at the top
-        $button.fadeIn().css('display', 'flex');
-      } else {
-        // At the top of the page
-        $button.fadeOut().css('display', 'flex');
-      }
-    });
-  }
+        $(window).scroll(function (event) {
+            var st = $(this).scrollTop();
+            if (st > 0) {
+                // Downscroll or not at the top
+                $button.fadeIn().css('display', 'flex');
+            } else {
+                // At the top of the page
+                $button.fadeOut().css('display', 'flex');
+            }
+        });
+    }
 });
